8e63e28f4e6b0c0c0cc2e081a7cbb069556fc45a
Ivan <icarpiodeveloper@gmail.com>
2026-02-21 16:37:58 +0100

    first commit

---
+++ config.js
@@ -0,0 +1,6 @@
+// config.js
+const CONFIG = {
+    API_URL: 'https://marianoapi.onrender.com',
+    // Cambiar a tu URL de producción cuando despliegues
+    // API_URL: 'https://tu-dominio.com'
+};
